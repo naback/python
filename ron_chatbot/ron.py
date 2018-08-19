d0fe2cb38387f528b1c895cabfeb7e55305dcf00
Naback <naback@live.com>
2018-08-18 21:00:17 -0300

    done

--- ron_chatbot/ron.py
+++ ron_chatbot/ron.py
@@ -1,24 +1,27 @@
 from chatterbot import ChatBot
 
+def train():
+	chatbot.train("chatterbot.corpus.english")
+
+def execute():
+	while 1:
+		question = input("Eu: ")
+		response = chatbot.get_response(question)
+		print("Ron: ", response)
+
 chatbot = ChatBot(
     'Ron',
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
 )
 
-print ("1 - Executar");
-print ("2 - Treinar");
+print ("1 - Execute");
+print ("9 - Train");
 op = input()
 
 if op == '1':
 	execute()
 
-it op == '2':
+if op == '9':
 	train()
 
-def train():
-	chatbot.train("chatterbot.corpus.english")
 
-def execute():
-	question = input("Eu: ")
-	response = chatbot.get_response(question)
-	print("Ron: ", response)
